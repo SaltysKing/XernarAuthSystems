8ef9d5625a3eff888b24e227e585f6fd78bde1e6
Glitch (glitch-hello-website) <none>
2022-04-21 04:26:40 +0000

    🏖🏉 Checkpoint ./index.html:12069183/6541 ./style.css:12069183/8016 ./script.js:12069183/2927

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+var upload = document.getElementById('upload');
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+function onFile() {
+    var me = this,
+        file = upload.files[0],
+        name = file.name.replace(/\.[^/.]+$/, '');
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+    if (file.type === '' ||
+        file.type === 'audio/mp3' ||       
+        file.type === 'file/text' ||
+        file.type === 'audio/mpeg' ||
+        file.type === 'audio/wav' ||
+        file.type === 'audio/x-wav' ||
+        file.type === 'audio/wave' ||
+        file.type === 'audio/x-pn-wav') {
+        if (file.size < (3000 * 1024)) {
+            upload.parentNode.className = 'area uploading';
+        } else {
+            window.alert('File size is too large, please ensure you are uploading a file of less than 3MB');
+        }
+    } else {
+        window.alert('File type ' + file.type + ' not supported');
+    }
 }
 
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+upload.addEventListener('dragenter', function (e) {
+    upload.parentNode.className = 'area dragging';
+}, false);
+
+upload.addEventListener('dragleave', function (e) {
+    upload.parentNode.className = 'area';
+}, false);
+
+upload.addEventListener('dragdrop', function (e) {
+    onFile();
+}, false);
+
+upload.addEventListener('change', function (e) {
+    onFile();
+}, false);
